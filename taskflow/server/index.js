d5983b4e9664ba8de971cf960946d7f0cac6bd1e
Keren Moreno <89100987+KerenMoreno16@users.noreply.github.com>
2026-05-12 17:10:40 -0300

    Update index.js

--- taskflow/server/index.js
+++ taskflow/server/index.js
@@ -18,6 +18,8 @@ mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log('MongoDB Connected'))
   .catch(err => console.log(err))
 
+console.log(process.env.MONGO_URI)
+
 app.listen(5000, () => {
   console.log('Server running')
 })
